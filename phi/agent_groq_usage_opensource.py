f432ab8e3817f9df1904338a8b2f78c36a25fc98
Prakash Kumar Thuthika <prakash.thuthika@gmail.com>
2025-02-22 23:14:59 +0530

    Update agent_groq_usage_opensource.py

--- phi/agent_groq_usage_opensource.py
+++ phi/agent_groq_usage_opensource.py
@@ -4,4 +4,9 @@
 from phi.agent import Agent
 from phi.model.groq import Groq
 
+agent = Agent(
+    # model = Groq(id="llama-3.1-8b-instant", api_key="******************")
+    model = Groq(id="deepseek-r1-distill-llama-70b", api_key="******************")
+)
+
 res = agent.print_response("Write a Poem on Pawan kalyan and CHiranjeevi?")
